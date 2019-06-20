71d7d45d0584d50324d5c86e3ed87bf9a20a60a3
Gabrielle Wicesawati Poerwawinata <gabybongbong@gmail.com>
2019-06-20 13:11:43 +0200

    props not defined

--- components/container/roleSelection.js
+++ components/container/roleSelection.js
@@ -2,10 +2,26 @@ import React, { Component } from 'react'
 import { Layout } from 'antd'
 import styled, { ThemeProvider } from 'styled-components'
 import theme from 'styled-theming'
+import { Button, Icon, Card, Row } from 'antd';
 
+
+const size = 'large'
 const RoleSelection = props => (
   <ThemeProvider theme={{ mode: props.theme }}>
     <StyledContent>
+    <Container>
+    <Row>
+      What would you like to do?
+    </Row>
+      <Button.Group size={size}>
+          <Button type="primary" onClick={this.props.RoleSelection.setRole("seeker")}>
+            Search for a room
+          </Button> 
+          <Button type="primary" onClick={this.props.RoleSelection.setRole("offerer")}>
+            Offer a room
+          </Button>
+        </Button.Group>
+      </Container>
     </StyledContent>
   </ThemeProvider>
 )
@@ -24,4 +40,11 @@ const StyledContent = styled(Layout.Content)`
   background-color: ${backgroundColor};
   color: ${textColor};
 `
+
+const Container = styled.div`
+  text-align: center;
+  margin-top: 10vh;
+  margin-bottom: 16vh;
+`
+
 export default RoleSelection
---
+++ stores/roleSelectionStore.js
@@ -0,0 +1,24 @@
+import {action, observable} from "mobx"
+import Router from "next/dist/client/router";
+import network from '../util/network';
+import { Blob } from 'react-blob';
+
+class Store {
+  @observable selection = {role: ''};
+
+  constructor() {
+    this.selection.role = '';
+  }
+
+  @action setRole = (role) => {
+  	console.log("setrole", role)
+
+  }
+
+
+
+}
+
+const initStore = () => new Store();
+
+export default initStore
