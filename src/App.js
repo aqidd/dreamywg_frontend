122fcc59e7bbe2656907a2c7acea2af57b9c62a3
Gabrielle Wicesawati Poerwawinata <gabybongbong@gmail.com>
2019-06-22 16:33:47 +0200

    still working on linkedin button css and response handling

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, BrowserRouter } from 'react-router-dom'
 import LandingScreen from './pages/landing'
 import ProfileSetupOfferer from './pages/profileSetupOfferer'
 import ProfileSetupSeeker from './pages/profileSetupSeeker'
+import SocialMediaAuth from './pages/socialMediaAuth'
 import Register from './pages/register'
 import Login from './pages/login'
 
@@ -14,6 +15,8 @@ const App = () => {
       <Route exact path="/login" component={Login} />
       <Route exact path="/setupofferer" component={ProfileSetupOfferer} />
       <Route exact path="/setupseeker" component={ProfileSetupSeeker} />
+      <Route exact path="/setupseeker" component={ProfileSetupSeeker} />
+      <Route exact path="/socialmediaauth" component={SocialMediaAuth} />
     </BrowserRouter>
   )
 }
