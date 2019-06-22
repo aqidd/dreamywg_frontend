9132c70dc7a423f4022bf894c44f44f3c5496436
Bukhori Aqid <aqid@Bukhoris-Air.fritz.box>
2019-06-22 13:56:36 +0200

    [delete] components module

--- components/common/customFooter.js
+++
@@ -1,6 +0,0 @@
-
-import React, {Component} from 'react'
-
-const CustomFooter = () => <div> custom footer </div>
-
-export default CustomFooter
--- components/common/form/wrappedAnyInput.js
+++
@@ -1,9 +0,0 @@
-
-
-const WrappedAnyInput = ({ tag, dec, objName, required }) =>
-dec(objName, {
-  rules: [{ required, message: 'Please complete this field!' }]
-})(tag)
-
-
-export default WrappedAnyInput
