ffba48a2cfe50a20a6732f8a8c81e35a489a3f81
ShubuhU <patipon74@gmail.com>
2019-07-03 22:03:54 +0200

    [+] add protected route

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React from 'react'
-import { Route, BrowserRouter } from 'react-router-dom'
+import { Route, BrowserRouter, Redirect } from 'react-router-dom'
 import LandingScreen from './pages/landing'
 import ProfileSetupOfferer from './pages/profileSetupOfferer'
 import ProfileSetupSeeker from './pages/profileSetupSeeker'
 import Register from './pages/register'
 import Login from './pages/login'
 
-const App = () => {
-  return (
-    <BrowserRouter>
-      <Route exact path="/" component={LandingScreen} />
-      <Route exact path="/register" component={Register} />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/setupofferer" component={ProfileSetupOfferer} />
-      <Route exact path="/setupseeker" component={ProfileSetupSeeker} />
-    </BrowserRouter>
-  )
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <BrowserRouter>
+        <Route exact path="/" component={LandingScreen} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/setupofferer" component={ProfileSetupOfferer} />
+        <Route exact path="/setupseeker" component={ProfileSetupSeeker} />
+      </BrowserRouter>
+    )
+  }
 }
 
-export default App
+const ProtectedRoute = ({ isAuth, ...others }) =>
+  isAuth ? <Route {...others} /> : <Redirect to="/login" />
