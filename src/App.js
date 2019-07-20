ddac400965cfc042671017927668b95710434466
stefan <stefand96@gmx.de>
2019-07-21 01:01:03 +0200

    fixed Bug again

--- src/App.js
+++ src/App.js
@@ -94,7 +94,7 @@ export default class App extends React.Component {
           exact
           path="/schedule/:id"
           Comp={Schedule}
-          isAuth={hasToken()}
+          isAuth={() => hasToken()}
         />
       </BrowserRouter>
     )
